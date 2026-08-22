55b875cae710b6b89da47b22c2e848cb68919ba2
Hamza Taj <hamzataj@Hamza-Al-Bulushi.local>
2026-08-22 08:21:18 +0400

    Add forward movement logic for pawns, one square in general, two squares for starting position

--- src/move/move-generator.cpp
+++ src/move/move-generator.cpp
@@ -1,11 +1,68 @@
 #include "move-generator.h"
 
+std::vector<Move> MoveGenerator::generatePawnMoves(Position& pos, int square) {
+    std::vector<Move> v;
+
+    // next move is current square number + 8 and -8 for white for going one square up and +16 and -16 for white for two squares up(first move only)
+    if(pos.sideToMove == WHITE) {
+        // calc destination, check if not empty and add to vector
+        int oneSquare = square - 8;
+        int twoSquare = square - 16;
+
+        // one square moves
+
+        if(0 <= oneSquare && oneSquare <= 63 && pos.piece[oneSquare] == EMPTY) {
+            
+            Move m;
+            m.from = square;
+            m.to = oneSquare;
+            v.push_back(m);
+            
+        }
+
+        // two square moves(starting position only: 48-55)
+        if(0 <= twoSquare && twoSquare <= 63 && pos.piece[twoSquare] == EMPTY && pos.piece[oneSquare] == EMPTY && 48 <= square && square <= 55) {
+            
+            Move m;
+            m.from = square;
+            m.to = twoSquare;
+            v.push_back(m);
+            
+        }
+        
+    }else {
+        // calc destination, check if not empty and add to vector
+        int oneSquare = square + 8;
+        int twoSquare = square + 16;
+
+        if(0 <= oneSquare && oneSquare <= 63 && pos.piece[oneSquare] == EMPTY) {
+
+            Move m;
+            m.from = square;
+            m.to = oneSquare;
+            v.push_back(m);
+
+
+        }
+
+         // two square moves(starting position only: 8-15)
+         if(0 <= twoSquare && twoSquare <= 63 && pos.piece[twoSquare] == EMPTY && pos.piece[oneSquare] == EMPTY && 8 <= square && square <= 15) {
+            
+            Move m;
+            m.from = square;
+            m.to = twoSquare;
+            v.push_back(m);
+            
+        }
+        
+    }
+
+    return v;
+}
 
 std::vector<Move> MoveGenerator::generateMoves(Position& pos) {
 
-    std::vector<Move> generatePawnMoves(Position&, int square) {
-        
-    }
+    
 
     std::vector<Move> v;
 
